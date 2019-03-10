b7ffcbd29c5bea49fb54f99e6c290406bcd68f4a
Mark Blue <mblue@students.mchenry.edu>
2019-03-10 15:31:34 -0500

    added dinosaur images added dinosaur skeleton images added urls in mainactivity

--- app/src/main/java/com/example/fossilsandskeletons/MainActivity.java
+++ app/src/main/java/com/example/fossilsandskeletons/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.fossilsandskeletons;
 
 import android.content.Context;
+import android.content.Intent;
 import android.media.Image;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -17,19 +20,40 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.t_rex,
             R.drawable.stegosaurus,
             R.drawable.triceratops,
+            R.drawable.velociraptor,
+            R.drawable.diplodocus
             };
     Integer[] Skeletons = {
             R.drawable.t_rex_skeleton,
             R.drawable.stegosaurus_skeleton,
-            R.drawable.triceratops_skeleton
+            R.drawable.triceratops_skeleton,
+            R.drawable.velociraptor_skeleton,
+            R.drawable.diplodocus_skeleton
+    };
+    enum Dino {
+            TREX,
+        STEG,
+        TRI,
+        VEL,
+        DIPLO
+    }
+    String[] Names = {
+            "T-rex",
+            "Stegosaurus",
+            "Triceratops",
+            "Velociraptor",
+            "Diplodocus"
     };
 
     ImageView pic;
+    Button btnLearn;
+    Dino dino;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLearn = findViewById(R.id.btnLearn);
 
         GridView grid = (GridView)findViewById(R.id.gridView);
         final ImageView pic = (ImageView)findViewById(R.id.imgLarge);
@@ -37,10 +61,37 @@ public class MainActivity extends AppCompatActivity {
         grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getBaseContext(),"Selected Species " + (position + 1), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getBaseContext(),Names[position], Toast.LENGTH_SHORT).show();
                 pic.setImageResource(Skeletons[position]);
+                dino = Dino.values()[position];
             }
         });
+        btnLearn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (dino){
+                    case TREX:
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.nhm.ac.uk/discover/dino-directory/tyrannosaurus.html")));
+                        break;
+                    case STEG:
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.nhm.ac.uk/discover/dino-directory/stegosaurus.html")));
+                        break;
+                    case TRI:
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.nhm.ac.uk/discover/dino-directory/triceratops.html")));
+                        break;
+                    case VEL:
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.nhm.ac.uk/discover/dino-directory/velociraptors.html")));
+                        break;
+                    case DIPLO:
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.nhm.ac.uk/discover/dino-directory/diplodocus.html")));
+                        break;
+                    default:
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.nhm.ac.uk/discover/dinosaurs.html")));
+                        break;
+                }
+            }
+        });
+
     }
     public class ImageAdapter extends BaseAdapter{
         private Context context;
